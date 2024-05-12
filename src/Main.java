2aaba503b906c9f2d52e0ad2370635cf1d4c2de7
Koss <kostuavavkon@mail.ru>
2024-05-12 16:00:24 +0300

    mm

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1");
+    var dog = 8;
+        {System.out.println(dog);}
+    var cat = 3.6;
+        {System.out.println(cat);}
+    var paper = 763789;
+        { System.out.println(paper);}
+
+        {System.out.println("2 задание");}
+
+        {System.out.println(dog + 4);}
+
+        {System.out.println(cat + 4);}
+
+        {System.out.println(paper + 4);}
+
+        {System.out.println("3 задание");}
+
+        {System.out.println(dog - 3.5);}
+        {System.out.println(cat - 1.6);}
+        {System.out.println(paper - 7639);}
+
+        {System.out.println("4 задание");}
+
+        var friend = 19;
+        {System.out.println(friend);}
+        {System.out.println(friend+2);}
+        {System.out.println((friend+2)-7);}
+
+        {System.out.println("5 задание");}
+
+        var frog = 3.5;
+        {System.out.println(frog);}
+        {System.out.println(frog*10);}
+        {System.out.println((frog*10)/3.5);}
+        {System.out.println(((frog*10)/3.5)+4);}
+
+        {System.out.println("6 задание");}
+
+var theBoxer1 = 78.2;
+var theBoxer2 = 82.7;
+        {System.out.println(theBoxer1 + theBoxer2);}
+        {System.out.println(theBoxer2 - theBoxer1);}
+
+        {System.out.println("7 задание");}
+
+        {System.out.println(theBoxer2 / theBoxer1);}
+
+        {System.out.println("8 задание");}
+
+
+        var allTime = 640 ;
+var workTime = 8;
+var result = 640/8;
+        {System.out.println("всего в компании работает"+  allTime/workTime +"человек");}
+
+var result2 = result+94 ;
+        {System.out.println(result+94);}
+var result3 = result2*8;
+        {System.out.println(result2*8);}
+    }
+}
